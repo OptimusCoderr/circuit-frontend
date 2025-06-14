a2a678fed7f0387557d01604496be5ac6a38d4c9
OptimusCoderr <chukwuebukaanulunko@gmail.com>
2025-06-14 18:12:45 +0100

    chnage in vite config

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base:"/circuit-frontend",
+  base:"/circuit-frontend/",
   plugins: [
     react(),
     mode === 'development' && componentTagger(),
