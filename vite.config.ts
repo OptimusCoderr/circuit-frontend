8d12b425a2a9ccbf873d407f18443776c9a5d1f8
Anulunko Chukwuebuka Oliver <chukwuebukaanulunko@gmail.com>
2025-06-14 20:40:10 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,18 +5,14 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base:"/circuit-frontend/",
+
   plugins: [
     react(),
-    mode === 'development' && componentTagger(),
+    mode === 'production' && componentTagger(),
   ].filter(Boolean),
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
-  },
-  build: {
-    outDir: 'dist',
-    sourcemap: false,
   },
 }));
